275109be4f38dbdc62488a5dcd5925f37d111b1e
Barry Allen <babanov-artem@yandex.ru>
2023-03-11 22:21:06 +0600

    add rest framework support

--- bookshop_back/settings.py
+++ bookshop_back/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'account',
     'books',
     'assortment',
     'service',
-    'support'
+    'support',
 ]
 
 MIDDLEWARE = [
